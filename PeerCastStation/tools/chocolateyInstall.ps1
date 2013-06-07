f49fd8e48a46df8582ee83de54a1d0f8d7579b83
progre <djyayutto@gmail.com>
2013-06-08 00:53:40 +0900

    add PeerCastStation

---
+++ PeerCastStation/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'PeerCastStation'
+$version = '1.4.0'
+
+$url = "http://www.pecastation.org/files/$packageName-$version.zip"
+$url64 = $url
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
+Install-ChocolateyDesktopLink "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\$packageName-$version\$packageName.exe"
+Write-ChocolateySuccess "$packageName"
---
+++ PeerCastStation/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'PeerCastStation'
+$version = '1.4.0'
+
+$zipFileName = "$packageName-$version.zip"
+$url = "http://www.pecastation.org/files/$zipFileName"
+$url64 = $url
+
+# UnInstall-ChocolateyZipPackage "$packageName" "$zipFileName"
+
+try {
+  $lnk = [Environment]::GetFolderPath('Desktop') + "\$packageName.exe.lnk"
+  if (Test-Path $lnk) {
+    Remove-Item $lnk
+  }
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
